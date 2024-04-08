5fa068b116330671a4972610075f0b2204e9a806
Mono Wallace <mono.wallace@gmail.com>
2024-04-08 16:53:10 +0200

    added sample app

---
+++ OrchardBlazor/OrchardCore.Modules/BlazorEnabledModule/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "BlazorEnabledModule",
+    Author = "Mono Wallace",
+    Website = "",
+    Version = "0.0.1",
+    Description = "BlazorEnabledModule",
+    Category = "Custom"
+)]
